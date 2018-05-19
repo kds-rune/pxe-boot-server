a448574f559ea87241ad68eae373598b393041d3
Rune Rønneseth <35924873+kds-rune@users.noreply.github.com>
2018-05-19 09:00:44 +0200

    Create launch-dnsmasq-basic.sh

---
+++ docker/launch-dnsmasq-basic.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo docker run -d --rm --cap-add=NET_ADMIN --network="host"  quay.io/coreos/dnsmasq \
+   --no-daemon \
+  --log-dhcp \
+  --log-queries
+  --dhcp-range=172.16.10.100,172.16.10.200,10m \
+  --dhcp-option=3,172.16.8.100 \
+  --dhcp-option=42,129.240.2.6 \
+  --no-resolv \
+  --strict-order \
+  --server=208.67.222.222 \
+  --server=208.67.220.220 \
+  --server=8.8.8.8 \
+  --server=8.8.4.4
